10d251181577e0b98f64e6de5b71695a944f4ba0
Giacomo <g.tombari2@campus.uniurb.it>
2021-12-23 15:57:18 +0100

    Interfaccia Menu: metodi e commenti

--- FoodDelivery/src/it/fooddelivery/model/Menu.java
+++ FoodDelivery/src/it/fooddelivery/model/Menu.java
@@ -4,4 +4,12 @@ package it.fooddelivery.model;
 
 public interface Menu {
 
+	// Return the price of the menu
+	double price();
+	// Return the name of the menu
+	String name();
+	// Return the size of the menu
+	int size();
+	// Return the info of the menu
+	String show();
 }
