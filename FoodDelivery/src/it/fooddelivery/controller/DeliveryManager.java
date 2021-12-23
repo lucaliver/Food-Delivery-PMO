3aabdb540776459734bffd7bd3afd5941c92ec0c
L. <l.procicchiani@campus.uniurb.it>
2021-12-23 12:32:48 +0100

    Aggiunti campi DeliveryManager

--- FoodDelivery/src/it/fooddelivery/controller/DeliveryManager.java
+++ FoodDelivery/src/it/fooddelivery/controller/DeliveryManager.java
@@ -2,6 +2,24 @@
 
 package it.fooddelivery.controller;
 
-public class DeliveryManager {
+import java.util.ArrayList;
+import java.util.List;
 
+import it.fooddelivery.model.Order;
+import it.fooddelivery.model.Restaurant;
+import it.fooddelivery.model.Zone;
+
+public class DeliveryManager {
+	private List<Order> waitingOrders = new ArrayList<>();
+	private List<Zone> zones = new ArrayList<>();
+	private List<Restaurant> restaurants = new ArrayList<>();
+	
+	//Costruttore
+	DeliveryManager(){
+		this.waitingOrders = new ArrayList<>();
+		this.zones = new ArrayList<>();
+		this.restaurants = new ArrayList<>();
+	}
+	
+	
 }
