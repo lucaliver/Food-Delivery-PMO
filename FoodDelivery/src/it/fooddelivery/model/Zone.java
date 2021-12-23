4518a4090532e1df4865f198759ae1081d38e585
Giulia Costa <g.costa14@campus.uniurb.it>
2021-12-23 12:36:55 +0100

    Creazione interfacce

--- FoodDelivery/src/it/fooddelivery/model/Zone.java
+++ FoodDelivery/src/it/fooddelivery/model/Zone.java
@@ -1,6 +1,21 @@
 package it.fooddelivery.model;
 
+import java.util.List;
+
 // Giulia Costa
 public enum Zone {
-
+	FERMIGNANO("Fermignano"),
+	CAGLI("Cagli"),
+	TAVULLIA("Tavullia"),
+	URBANIA("Urbania"),
+	FOSSOMBRONE("Fossombrone");
+	
+	
+	private String description;
+	
+	List<DeliveryMan> deliveryMan();
+	
+	public String toString() {
+		return description;
+	}
 }
