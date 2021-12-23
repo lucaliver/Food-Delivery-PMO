21214404083bf1e9f45fbb6e22c03486e8905b4b
L. <l.procicchiani@campus.uniurb.it>
2021-12-23 11:55:35 +0100

    Ci siamo

--- FoodDelivery/src/it/fooddelivery/model/Restaurant.java
+++ FoodDelivery/src/it/fooddelivery/model/Restaurant.java
@@ -1,12 +1,5 @@
 package it.fooddelivery.model;
 
 public interface Restaurant {
-	// COMMENTO DI LUCA
-<<<<<<< HEAD
-	// commento di Giulia
-=======
-	// Commento di Giacomo
-	// Capire come cavolo funziona git
->>>>>>> branch 'master' of https://github.com/lucaliver/Food-Delivery-PMO
-	// NUOVO COMMIT GIULIA
+	// Questo è un commento
 }
