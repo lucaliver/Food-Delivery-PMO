0d1962c4203cc66897f4864ca509855b33838885
Giacomo <g.tombari2@campus.uniurb.it>
2021-12-27 14:23:49 +0100

    prova modifiche

--- FoodDelivery/src/it/fooddelivery/model/implementation/OrderImpl.java
+++ FoodDelivery/src/it/fooddelivery/model/implementation/OrderImpl.java
@@ -12,12 +12,14 @@ public class OrderImpl implements Order{
 	private final String idOrder;
 	private final ZoneImpl destination;
 	private final List<Menu> menus;
+	private final int x;
 	
 	// Costruttore
 	public OrderImpl(String id, ZoneImpl dest) {		
 		this.idOrder = id;
 		this.destination = dest;
 		this.menus = new ArrayList<>();
+		this.x = 1;
 	}
 	
 	
