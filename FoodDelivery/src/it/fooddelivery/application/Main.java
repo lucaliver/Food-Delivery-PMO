3abe45d49deb403e176ac6d09d9ee7b39a790fbe
L. <l.procicchiani@campus.uniurb.it>
2022-01-02 11:51:19 +0100

    Due righe su main.

--- FoodDelivery/src/it/fooddelivery/application/Main.java
+++ FoodDelivery/src/it/fooddelivery/application/Main.java
@@ -11,9 +11,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		final DeliveryManager controller = new DeliveryManager();
-		final View view = new View(controller);
-		
-		//TODO Finire main.
+
+		new View(controller);
 	}
 
 }
