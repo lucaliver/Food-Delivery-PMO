74517598a86b417ba0f59e61f04de37783dfb1f3
Giacomo <g.tombari2@campus.uniurb.it>
2022-01-18 15:27:35 +0100

    Prova correzione d'errore nelle view

--- FoodDelivery/src/it/fooddelivery/view/View.java
+++ FoodDelivery/src/it/fooddelivery/view/View.java
@@ -10,7 +10,6 @@ import it.fooddelivery.controller.Manager;
  * View component for MVC pattern of the delivery management software.
  */
 public class View {
-	ViewForWorker vfworker;
 	
 	/**
 	 * It creates the first windows for the customer and the window for the workers.
